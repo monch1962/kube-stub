8fd332a2ba5f02e26301306ab0a67fd0b9e9be3d
David Mitchell <monch1962@gmail.com>
2020-05-30 18:13:28 +1000

    Initial commit

---
+++ deploy-stub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z ${NAMESPACE} ]; then
+	echo "NAMESPACE environment variable needs to be set"
+	exit 1
+fi
+
+if [ -z ${STUB_CONFIG} ]; then
+	echo "STUB_CONFIG environment variable needs to be set"
+	exit 1
+fi
+
+kubectl create namespace $NAMESPACE
+kubectl apply -f stub-config.yaml --namespace $NAMESPACE
